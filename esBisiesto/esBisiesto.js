38de9078b8642bc83231f4ca04e5324a0f5b63ed
jonathanbarzola <jonabl2102@gmail.com>
2022-12-02 15:27:53 -0500

    Algoritmo año bisiesto

--- esBisiesto/esBisiesto.js
+++ esBisiesto/esBisiesto.js
@@ -1,32 +1,15 @@
 function esBisiesto(year) {
 
-    // Validamos que el año sea divisible entre 4, si NO es divisible entonces retornamos false.
-    if (year % 4 === 0) {
-        
-        // Si el año NO es divisible entre 100 entonces el año es bisiesto
-        if (year % 100 === 0) {
-            
-            if (year % 400 === 0) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return true;
-        }
-    
-    } else {
-        return false;
-    }
-
-}
-
-function isLeapYear(year) {
+  // Verificamos que año sea divisibible entre 4, si NO es divisible entre 4 entonces NO es un año bisiesto.
   if (year % 4 === 0) {
+
+    // Si si es divisible por 4 pero el año es divisible entre 100 entonces NO es un año bisiesto
     if (year % 100 === 0) {
+
+      // Si es divisible por 4 y 400 entonces es un año bisiesto :D
       if (year % 400 === 0) {
         return true;
-      } else {
+      } else {       
         return false;
       }
     } else {
@@ -36,4 +19,5 @@ function isLeapYear(year) {
     return false;
   }
 }
-  
+
+console.log(esBisiesto(1900))
